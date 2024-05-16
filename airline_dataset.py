fee1450c2ef6043c143882e2c44fa221ca4582ee
lamossta <lamossta@fel.cvut.cz>
2024-05-16 11:00:05 +0200

    added get edges

--- airline_dataset.py
+++ airline_dataset.py
@@ -9,11 +9,15 @@ class AirlineDataset:
     def load_data(self):
         self.G = nx.read_graphml(self.dataset_path)
 
-    def get_data_points(self) -> list:
+    def get_nodes(self) -> list:
         return [node for node in self.G.nodes(data=True)]
+
+    def get_edges(self) -> list:
+        return [edge for edge in self.G.edges(data=True)]
 
 
 if __name__ == "__main__":
     airline_dataset = AirlineDataset()
     airline_dataset.load_data()
-    print(airline_dataset.get_data_points())
+    print(airline_dataset.get_nodes())
+    print(airline_dataset.get_edges())
