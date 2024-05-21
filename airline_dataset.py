40d41bd8fb33f1682522fba0be1d87d057025af3
cernyad <AdamCerny77@gmail.com>
2024-05-21 13:13:13 +0200

    improved node format

--- airline_dataset.py
+++ airline_dataset.py
@@ -1,17 +1,48 @@
 import networkx as nx
 import xml.etree.ElementTree as ET
+import re
 
 
 class AirlineDataset:
-    def __init__(self):
-        self.dataset_path = "./data/airlines.graphml"
+    def __init__(self, path):
+        self.dataset_path = path
         self.edges = self._get_edges()
         self.n_edges = len(self.edges)
         self.nodes = self._get_nodes()
         self.n_nodes = len(self.nodes)
 
     def _get_nodes(self) -> list:
-        return [node for node in nx.read_graphml(self.dataset_path).nodes(data=True)]
+        def convert_format(node_data):
+            tooltip = node_data['tooltip']
+
+            # Extracting the name, longitude, and latitude using regex
+            name_match = re.search(r'([A-Z]+)\(lngx', tooltip)
+            coord_match = re.search(r'lngx=([-+]?\d*\.\d+|\d+),laty=([-+]?\d*\.\d+|\d+)', tooltip)
+
+            if name_match and coord_match:
+                name = name_match.group(1)
+                longitude = float(coord_match.group(1))
+                latitude = float(coord_match.group(2))
+            else:
+                raise ValueError("Invalid format for tooltip")
+
+            return {
+                'name': name,
+                'latitude': latitude,
+                'longitude': longitude
+            }
+
+        graph = nx.read_graphml(self.dataset_path)
+        nodes = []
+
+        for node, data in graph.nodes(data=True):
+            if 'tooltip' in data:
+                formatted_node = convert_format(data)
+                nodes.append(formatted_node)
+            else:
+                raise ValueError(f"Node {node} does not contain 'tooltip' attribute")
+
+        return nodes
 
     def _get_edges(self) -> list:
         with open(self.dataset_path, 'r') as file:
@@ -27,3 +58,8 @@ class AirlineDataset:
             edges.append((source, target))
 
         return edges
+
+if __name__ == '__main__':
+    dataset = AirlineDataset("data/airlines.graphml")
+    print(dataset.nodes[:10])
+    print(dataset.edges[:10])
